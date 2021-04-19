a7d4bf99ecc2f6e3c479c68db7bdc2ce3c06c42c
Abhishek Banerjee <abhishek.banerjee18@outlook.com>
2021-04-19 10:08:36 +0530

    Added Python-Decouple

--- recipeapp/settings.py
+++ recipeapp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(*khudq=3j=w#wg@=_1_5ntdl+n^o6-bu@9rhtwz2&^j23bq4e'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = confi('SECRET_KEY')
+
+
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
